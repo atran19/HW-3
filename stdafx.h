cf1c05b3fd5a9dba2b22de48fc203ea863611e70
atran19 <atran19@my.whitworth.edu>
2017-03-13 21:03:58 -0700

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#ifndef STDAFX_H_
+#define STDAFX_H_
+
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
+#endif
